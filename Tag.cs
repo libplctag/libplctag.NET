c34ce879bbe3ec011d9d97aee672ba857c769d4d
timyhac <timyhac@gmail.com>
2019-09-01 19:38:19 +1000

    Minor refactor

--- Tag.cs
+++ Tag.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
 
 namespace libplctag
 {
@@ -20,7 +19,7 @@ namespace libplctag
         public int DebugLevel { get; }
         public TimeSpan DefaultTimeout { get; }
 
-        private int _pointer;
+        private int pointer;
 
         /// <summary>
         /// Factory method to create a tag. If the CPU type is LGX, the port type and slot has to be specified.
@@ -49,7 +48,7 @@ namespace libplctag
 
             var attributeString = GetAttributeString(protocol, gateway, path, cpuType, elementSize, elementCount, name, debugLevel);
 
-            _pointer = Dll.plc_tag_create(attributeString, (int)defaultTimeout.TotalMilliseconds); ;
+            pointer = Dll.plc_tag_create(attributeString, (int)defaultTimeout.TotalMilliseconds); ;
 
         }
 
@@ -83,47 +82,47 @@ namespace libplctag
 
         }
 
-        public void Dispose() => Dll.plc_tag_destroy(_pointer);
+        public void Dispose() => Dll.plc_tag_destroy(pointer);
 
-        public void Abort() => Dll.plc_tag_abort(_pointer);
+        public void Abort() => Dll.plc_tag_abort(pointer);
 
-        public void Read(TimeSpan timeout) => Dll.plc_tag_read(_pointer, (int)timeout.TotalMilliseconds);
+        public void Read(TimeSpan timeout) => Dll.plc_tag_read(pointer, (int)timeout.TotalMilliseconds);
 
-        public void Write(TimeSpan timeout) => Dll.plc_tag_write(_pointer, (int)timeout.TotalMilliseconds);
+        public void Write(TimeSpan timeout) => Dll.plc_tag_write(pointer, (int)timeout.TotalMilliseconds);
 
-        public int GetSize() => Dll.plc_tag_get_size(_pointer);
+        public int GetSize() => Dll.plc_tag_get_size(pointer);
 
-        public StatusCode GetStatus() => (StatusCode)Dll.plc_tag_status(_pointer);
+        public StatusCode GetStatus() => (StatusCode)Dll.plc_tag_status(pointer);
 
-        public ulong GetUInt64(int offset) => Dll.plc_tag_get_uint64(_pointer, offset);
-        public void SetUInt64(int offset, ulong value) => Dll.plc_tag_set_uint64(_pointer, offset, value);
+        public ulong GetUInt64(int offset) => Dll.plc_tag_get_uint64(pointer, offset);
+        public void SetUInt64(int offset, ulong value) => Dll.plc_tag_set_uint64(pointer, offset, value);
 
-        public long GetInt64(int offset) => Dll.plc_tag_get_int64(_pointer, offset);
-        public void SetInt64(int offset, long value) => Dll.plc_tag_set_int64(_pointer, offset, value);
+        public long GetInt64(int offset) => Dll.plc_tag_get_int64(pointer, offset);
+        public void SetInt64(int offset, long value) => Dll.plc_tag_set_int64(pointer, offset, value);
 
-        public uint GetUInt32(int offset) => Dll.plc_tag_get_uint32(_pointer, offset);
-        public void SetUInt32(int offset, uint value) => Dll.plc_tag_set_uint32(_pointer, offset, value);
+        public uint GetUInt32(int offset) => Dll.plc_tag_get_uint32(pointer, offset);
+        public void SetUInt32(int offset, uint value) => Dll.plc_tag_set_uint32(pointer, offset, value);
 
-        public int GetInt32(int offset) => Dll.plc_tag_get_int32(_pointer, offset);
-        public void SetInt32(int offset, int value) => Dll.plc_tag_set_int32(_pointer, offset, value);
+        public int GetInt32(int offset) => Dll.plc_tag_get_int32(pointer, offset);
+        public void SetInt32(int offset, int value) => Dll.plc_tag_set_int32(pointer, offset, value);
 
-        public ushort GetUInt16(int offset) => Dll.plc_tag_get_uint16(_pointer, offset);
-        public void SetUInt16(int offset, ushort value) => Dll.plc_tag_set_uint16(_pointer, offset, value);
+        public ushort GetUInt16(int offset) => Dll.plc_tag_get_uint16(pointer, offset);
+        public void SetUInt16(int offset, ushort value) => Dll.plc_tag_set_uint16(pointer, offset, value);
 
-        public short GetInt16(int offset) => Dll.plc_tag_get_int16(_pointer, offset);
-        public void SetInt16(int offset, short value) => Dll.plc_tag_set_int16(_pointer, offset, value);
+        public short GetInt16(int offset) => Dll.plc_tag_get_int16(pointer, offset);
+        public void SetInt16(int offset, short value) => Dll.plc_tag_set_int16(pointer, offset, value);
 
-        public byte GetUInt8(int offset) => Dll.plc_tag_get_uint8(_pointer, offset);
-        public void SetUInt8(int offset, byte value) => Dll.plc_tag_set_uint8(_pointer, offset, value);
+        public byte GetUInt8(int offset) => Dll.plc_tag_get_uint8(pointer, offset);
+        public void SetUInt8(int offset, byte value) => Dll.plc_tag_set_uint8(pointer, offset, value);
 
-        public sbyte GetInt8(int offset) => Dll.plc_tag_get_int8(_pointer, offset);
-        public void SetInt8(int offset, sbyte value) => Dll.plc_tag_set_int8(_pointer, offset, value);
+        public sbyte GetInt8(int offset) => Dll.plc_tag_get_int8(pointer, offset);
+        public void SetInt8(int offset, sbyte value) => Dll.plc_tag_set_int8(pointer, offset, value);
 
-        public double GetFloat64(int offset) => Dll.plc_tag_get_float64(_pointer, offset);
-        public void SetFloat64(int offset, double value) => Dll.plc_tag_set_float64(_pointer, offset, value);
+        public double GetFloat64(int offset) => Dll.plc_tag_get_float64(pointer, offset);
+        public void SetFloat64(int offset, double value) => Dll.plc_tag_set_float64(pointer, offset, value);
 
-        public float GetFloat32(int offset) => Dll.plc_tag_get_float32(_pointer, offset);
-        public void SetFloat32(int offset, float value) => Dll.plc_tag_set_float32(_pointer, offset, value);
+        public float GetFloat32(int offset) => Dll.plc_tag_get_float32(pointer, offset);
+        public void SetFloat32(int offset, float value) => Dll.plc_tag_set_float32(pointer, offset, value);
 
     }
 
