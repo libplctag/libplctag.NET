89460c9b294bbdcf21d76e1a676c220a12c95a00
timyhac <timyhac@gmail.com>
2019-09-01 14:07:15 +1000

    Added other allowable Cpu types

--- CpuType.cs
+++ CpuType.cs
@@ -2,7 +2,13 @@
 {
     public enum CpuType
     {
+        COMPACTLOGIX,
+        CLGX,
         LGX,
+        CONTROLLOGIX,
+        CONTROLOGIX,
+        FLEXLOGIX,
+        FLGX,
         SLC,
         PLC5
     }
