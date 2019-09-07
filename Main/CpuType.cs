346fced339a6d2cc3dce6780a31035ce029cac48
Tim Cantle <TimCantle@gmail.com>
2019-09-07 14:56:18 +1000

    Merged NativeImport and main library

---
+++ Main/CpuType.cs
@@ -0,0 +1,15 @@
+﻿namespace libplctag
+{
+    public enum CpuType
+    {
+        COMPACTLOGIX,
+        CLGX,
+        LGX,
+        CONTROLLOGIX,
+        CONTROLOGIX,
+        FLEXLOGIX,
+        FLGX,
+        SLC,
+        PLC5
+    }
+}
