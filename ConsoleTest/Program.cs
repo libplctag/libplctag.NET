892589b4c813062a2d3e5886fe5bba3e91482dd4
jkoplo <jkoplo@users.noreply.github.com>
2020-06-18 00:05:34 -0700

    Example console code added
    * Just a copy of timyhac example in the README

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using libplctag;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace ExampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myTag = new Tag(IPAddress.Parse("10.10.10.10"), "1,0", CpuTypes.LGX, DataTypes.DINT, "PROGRAM:SomeProgram.SomeDINT");
+            while (myTag.GetStatus() == StatusCode.PLCTAG_STATUS_PENDING)
+            {
+                Thread.Sleep(100);
+            }
+            myTag.ThrowIfError();
+
+            myTag.SetInt32(0, 3737);
+            myTag.Write(TimeSpan.Zero);
+            while (myTag.GetStatus() == StatusCode.PLCTAG_STATUS_PENDING)
+            {
+                Thread.Sleep(100);
+            }
+            myTag.ThrowIfError();
+
+
+            myTag.Read(TimeSpan.Zero);
+            while (myTag.GetStatus() == StatusCode.PLCTAG_STATUS_PENDING)
+            {
+                Thread.Sleep(100);
+            }
+            myTag.ThrowIfError();
+
+            int myDint = myTag.GetInt32(0);
+
+
+            Console.WriteLine(myDint);
+
+        }
+    }
+}
