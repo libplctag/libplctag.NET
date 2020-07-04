f5b2119cf78841e6b0dd92617074ce5607578293
timyhac <timyhac@gmail.com>
2020-07-04 18:52:19 +1000

    Added events Enum

---
+++ src/libplctag/Events.cs
@@ -0,0 +1,12 @@
+﻿namespace libplctag
+{
+    public enum Events
+    {
+        ReadStarted = 1,
+        ReadCompleted = 2,
+        WriteStarted = 3,
+        WriteCompleted = 4,
+        Aborted = 5,
+        Destroyed = 6
+    }
+}
