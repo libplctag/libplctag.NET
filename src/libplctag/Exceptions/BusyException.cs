b8477f7e1fb401cd4961e5440ca8fc560a82460c
timyhac <timyhac@gmail.com>
2020-07-04 18:41:31 +1000

    Added additional Exception types for new statuses

---
+++ src/libplctag/Exceptions/BusyException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace libplctag
+{
+
+    public class BusyException : Exception
+    {
+        public BusyException()
+        {
+        }
+
+        public BusyException(string message)
+            : base(message)
+        {
+        }
+
+        public BusyException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
