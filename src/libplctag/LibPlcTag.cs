888addf111318aed373a57bbc2687e7d5b2dcdeb
timyhac <timyhac@gmail.com>
2020-07-06 19:24:00 +1000

    Slightly more readable - no magic numbers

--- src/libplctag/LibPlcTag.cs
+++ src/libplctag/LibPlcTag.cs
@@ -5,9 +5,11 @@ namespace libplctag
     public static class LibPlcTag
     {
 
-        static public int VersionMajor => plctag.get_int_attribute(0, "version_major", int.MinValue);
-        static public int VersionMinor => plctag.get_int_attribute(0, "version_minor", int.MinValue);
-        static public int VersionPatch => plctag.get_int_attribute(0, "version_patch", int.MinValue);
+        static int libraryAttributePointer = 0;
+
+        static public int VersionMajor => plctag.get_int_attribute(libraryAttributePointer, "version_major", int.MinValue);
+        static public int VersionMinor => plctag.get_int_attribute(libraryAttributePointer, "version_minor", int.MinValue);
+        static public int VersionPatch => plctag.get_int_attribute(libraryAttributePointer, "version_patch", int.MinValue);
         static public bool IsRequiredVersion(int requiredMajor, int requiredMinor, int requiredPatch)
         {
             var result = (StatusCode)plctag.check_lib_version(requiredMajor, requiredMinor, requiredPatch);
