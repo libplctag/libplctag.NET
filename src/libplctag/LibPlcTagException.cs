e79335648730712058ce0aa2fc197fb6f7c9b852
timyhac <timyhac@gmail.com>
2020-07-06 19:41:38 +1000

    Consolidated all exception types into a single LibPlcTagException with descriptions

---
+++ src/libplctag/LibPlcTagException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace libplctag
+{
+
+    public class LibPlcTagException : Exception
+    {
+        public LibPlcTagException()
+        {
+        }
+
+        public LibPlcTagException(string message)
+            : base(message)
+        {
+        }
+
+        public LibPlcTagException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
