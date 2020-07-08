00178c5608b06de9a37b1beacd579cb1da747c33
timyhac <timyhac@gmail.com>
2020-07-08 15:23:21 +1000

    Added VB.NET Example and .NET Framework, restructured examples

--- src/Examples/CSharp DotNetCore/NativeImportExample.cs
+++ src/Examples/CSharp DotNetCore/NativeImportExample.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 using libplctag.NativeImport;
 
-namespace ExampleConsoleApp
+namespace CSharpDotNetCore
 {
     class NativeImportExample
     {
---
+++ src/Examples/CSharp DotNetFramework/Program.cs
@@ -0,0 +1,40 @@
+﻿using libplctag;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace CSharpDotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myTag = new Tag(IPAddress.Parse("192.168.0.10"), "1,0", CpuType.Logix, DataType.DINT, "MY_DINT");
+
+            while (myTag.GetStatus() == StatusCode.StatusPending)
+                Thread.Sleep(100);
+            if (myTag.GetStatus() != StatusCode.StatusOk)
+                throw new LibPlcTagException(myTag.GetStatus());
+
+            myTag.SetInt32(0, 3737);
+            myTag.Write(TimeSpan.Zero);
+
+            while (myTag.GetStatus() == StatusCode.StatusPending)
+                Thread.Sleep(100);
+            if (myTag.GetStatus() != StatusCode.StatusOk)
+                throw new LibPlcTagException(myTag.GetStatus());
+
+            myTag.Read(TimeSpan.Zero);
+            while (myTag.GetStatus() == StatusCode.StatusPending)
+                Thread.Sleep(100);
+            if (myTag.GetStatus() != StatusCode.StatusOk)
+                throw new LibPlcTagException(myTag.GetStatus());
+
+            int myDint = myTag.GetInt32(0);
+
+            Console.WriteLine(myDint);
+
+        }
+    }
+}
