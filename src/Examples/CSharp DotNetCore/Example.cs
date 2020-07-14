5704429939dc72f99d62ac1d4ff869e82537620d
timyhac <timyhac@gmail.com>
2020-07-14 18:09:07 +1000

    Updated example to work with change to Status/StatusCode

--- src/Examples/CSharp DotNetCore/Example.cs
+++ src/Examples/CSharp DotNetCore/Example.cs
@@ -11,24 +11,24 @@ namespace CSharpDotNetCore
         {
             var myTag = new Tag(IPAddress.Parse("10.10.10.10"), "1,0", CpuType.Logix, DataType.DINT, "PROGRAM:SomeProgram.SomeDINT", 5000);
 
-            while (myTag.GetStatus() == StatusCode.StatusPending)
+            while (myTag.GetStatus() == Status.Pending)
                 Thread.Sleep(100);
-            if (myTag.GetStatus() != StatusCode.StatusOk)
+            if (myTag.GetStatus() != Status.Ok)
                 throw new LibPlcTagException(myTag.GetStatus());
 
             myTag.SetInt32(0, 3737);
             myTag.Write(0);
 
-            while (myTag.GetStatus() == StatusCode.StatusPending)
+            while (myTag.GetStatus() == Status.Pending)
                 Thread.Sleep(100);
-            if (myTag.GetStatus() != StatusCode.StatusOk)
+            if (myTag.GetStatus() != Status.Ok)
                 throw new LibPlcTagException(myTag.GetStatus());
 
             myTag.Read(0);
 
-            while (myTag.GetStatus() == StatusCode.StatusPending)
+            while (myTag.GetStatus() == Status.Pending)
                 Thread.Sleep(100);
-            if (myTag.GetStatus() != StatusCode.StatusOk)
+            if (myTag.GetStatus() != Status.Ok)
                 throw new LibPlcTagException(myTag.GetStatus());
 
             int myDint = myTag.GetInt32(0);
