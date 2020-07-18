e50bbde0772b573a5c2d471eda29b05d48ec1eb9
timyhac <timyhac@gmail.com>
2020-07-18 17:36:27 +1000

    Added example for Timer

--- src/Examples/CSharp DotNetCore/ExampleGenericTag.cs
+++ src/Examples/CSharp DotNetCore/ExampleGenericTag.cs
@@ -11,6 +11,7 @@ namespace CSharpDotNetCore
     {
         public static void Run()
         {
+
             var MyDintArray = new Tag<DINTIntMarshaller, int>(IPAddress.Parse("192.168.0.10"), "1,0", CpuType.Logix, "MY_DINT_ARRAY_1000[0]", 1000, 1000);
 
             for (int ii = 0; ii < 1000; ii++)
@@ -18,12 +19,28 @@ namespace CSharpDotNetCore
 
             MyDintArray.Write(1000);
 
+
+
+
             var MyStringArray = new Tag<STRINGStringMarshaller, string>(IPAddress.Parse("192.168.0.10"), "1,0", CpuType.Logix, "MY_STRING_ARRAY_10[0]", 1000, 10);
 
             for (int ii = 0; ii < 10; ii++)
-                MyStringArray[ii] = (ii*1111).ToString();
+                MyStringArray[ii] = (ii * 1111).ToString();
 
             MyStringArray.Write(1000);
+
+
+
+
+            var MyTimer = new Tag<TIMERTimerMarshaller, AbTimer>(IPAddress.Parse("192.168.0.10"), "1,0", CpuType.Logix, "MY_TIMER", 1000);
+            MyTimer.Read(1000);
+            Console.WriteLine(
+                $"Preset: {MyTimer[0].Preset}\n" +
+                $"Accumulated: {MyTimer[0].Accumulated}\n" +
+                $"Enabled: {MyTimer[0].Enabled}\n" +
+                $"InProgress: {MyTimer[0].InProgress}\n" +
+                $"Done: {MyTimer[0].Done}"
+                );
 
         }
     }
---
+++ src/libplctag/DataTypes/TIMERTimerMarshaller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace libplctag.DataTypes
+{
+    public class TIMERTimerMarshaller: IMarshaller<AbTimer>
+    {
+
+        public int ElementSize => 12;
+
+        public AbTimer Decode(Tag tag, int index)
+        {
+
+            // Needed to look at RsLogix documentation for structure of TIMER
+            var DINT2 = tag.GetInt32(ElementSize * index);
+            var DINT1 = tag.GetInt32(ElementSize * index + 4);
+            var DINT0 = tag.GetInt32(ElementSize * index + 8);
+
+            // The third DINT packs a few BOOLs into it
+            var bitArray = new BitArray(new int[] { DINT2 });
+
+            var timer = new AbTimer
+            {
+                Accumulated = DINT0,         // ACC
+                Preset = DINT1,              // PRE
+                Done = bitArray[29],         // DN
+                InProgress = bitArray[30],   // TT
+                Enabled = bitArray[31]       // EN
+            };
+
+            return timer;
+
+        }
+
+        public void Encode(Tag tag, int index, AbTimer value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AbTimer
+    {
+        public int Preset { get; set; }
+        public int Accumulated { get; set; }
+        public bool Enabled { get; set; }
+        public bool InProgress { get; set; }
+        public bool Done { get; set; }
+    }
+}
