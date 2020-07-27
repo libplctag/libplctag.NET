98628a47b5c2977015ee1a048c785d50e4ac83f2
timyhac <timyhac@gmail.com>
2020-07-27 22:29:09 +1000

    More cleanup

--- src/Examples/CSharp DotNetCore/ExampleListTags.cs
+++ src/Examples/CSharp DotNetCore/ExampleListTags.cs
@@ -16,15 +16,13 @@ namespace CSharpDotNetCore
 
         public static void Run()
         {
-            var host = "192.168.0.10";
-            var path = "1,0";
 
             // The current constructor forces us to choose the element count, but @tags is special
             // so ideally we don't supply elem_count in the attribute string
             // For now, this works as long as there are at least 5 tags in the controller
             var elementCount = 5;
 
-            var tags = new Tag<TagListingMarshaller, TagInfo>(IPAddress.Parse(host), path, PlcType.ControlLogix, "@tags", TIMEOUT_MS, elementCount);
+            var tags = new Tag<TagListingMarshaller, TagInfo>(IPAddress.Parse("10.10.10.10"), "1,0", PlcType.ControlLogix, "@tags", TIMEOUT_MS, elementCount);
 
             ConsoleTable
                 .From(tags.Value.Select(t => new { t.Id, Type = $"0x{t.Type:X}", t.Name, t.Length, Dimensions = string.Join(',', t.Dimensions) }))
@@ -47,14 +45,14 @@ namespace CSharpDotNetCore
     public class TagListingMarshaller : IMarshaller<TagInfo>
     {
 
+        const int TAG_STRING_SIZE = 200;
+
         public int ElementSize => 0;
 
         public PlcType PlcType { get; set; }
 
         public TagInfo Decode(Tag tag, int offset, out int elementSize)
         {
-
-            var TAG_STRING_SIZE = 200;
 
             var tagInstanceId = tag.GetUInt32(offset);
             var tagType = tag.GetUInt16(offset+4);
@@ -66,8 +64,8 @@ namespace CSharpDotNetCore
                 tag.GetUInt32(offset + 16)
             };
 
-            var apparentTagNameLength = tag.GetUInt16(offset + 20);
-            var actualTagNameLength = (int)Math.Min(apparentTagNameLength, TAG_STRING_SIZE * 2 - 1);
+            var apparentTagNameLength = (int)tag.GetUInt16(offset + 20);
+            var actualTagNameLength = Math.Min(apparentTagNameLength, TAG_STRING_SIZE * 2 - 1);
 
             var tagNameBytes = Enumerable.Range(offset + 22, actualTagNameLength)
                 .Select(o => tag.GetUInt8(o))
