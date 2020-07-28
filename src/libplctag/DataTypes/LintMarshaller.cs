67b72aec56e318b43d16f614e3008daeb2ee76c8
timyhac <timyhac@gmail.com>
2020-07-28 21:34:24 +1000

    Added two additonal types, LINT and LREAL

---
+++ src/libplctag/DataTypes/LintMarshaller.cs
@@ -0,0 +1,19 @@
+﻿namespace libplctag.DataTypes
+{
+    public class LintMarshaller : IMarshaller<long>
+    {
+        private const int ElementSize = 8;
+        public PlcType PlcType { get; set; }
+        public long Decode(Tag tag, int offset, out int elementSize)
+        {
+            elementSize = ElementSize;
+            return tag.GetInt64(offset);
+        }
+
+        public void Encode(Tag tag, int offset, out int elementSize, long value)
+        {
+            elementSize = ElementSize;
+            tag.SetInt64(offset, value);
+        }
+    }
+}
---
+++ src/libplctag/DataTypes/LrealMarshaller.cs
@@ -0,0 +1,19 @@
+﻿namespace libplctag.DataTypes
+{
+    public class LrealMarshaller : IMarshaller<double>
+    {
+        private const int ElementSize = 4;
+        public PlcType PlcType { get; set; }
+        public double Decode(Tag tag, int offset, out int elementSize)
+        {
+            elementSize = ElementSize;
+            return tag.GetFloat64(offset);
+        }
+
+        public void Encode(Tag tag, int offset, out int elementSize, double value)
+        {
+            elementSize = ElementSize;
+            tag.SetFloat64(offset, value);
+        }
+    }
+}
