9f6de40d5cef0b72ee9a141d92b3bb88cba2321b
jkoplo <jkoplo@users.noreply.github.com>
2020-08-17 00:45:13 -0700

    Moving more implementation into the base marshaller
    This lightens our other marshallers at the expense of making them a little less flexible

--- src/libplctag/DataTypes/Marshaller.cs
+++ src/libplctag/DataTypes/Marshaller.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace libplctag.DataTypes
 {
-    public abstract class Marshaller<T>
+    public abstract class Marshaller<T>: IMarshaller<T>, IMarshaller<T[]>
     {
         public PlcType PlcType { get; set; }
+
+        abstract public int? ElementSize { get; }
 
         virtual public int? SetArrayLength(int? elementCount) => elementCount;
 
         virtual public int? GetArrayLength(Tag tag) => tag.ElementCount;
 
 
-        virtual protected T[] DecodeArray(Tag tag, int elementSize)
+        virtual protected T[] DecodeArray(Tag tag)
         {
+            if (ElementSize is null)
+            {
+                throw new ArgumentNullException($"{nameof(ElementSize)} cannot be null for array decoding");
+            }
 
             var buffer = new List<T>();
 
@@ -22,29 +29,38 @@ namespace libplctag.DataTypes
             while (offset < tagSize)
             {
                 buffer.Add(Decode(tag, offset));
-                offset += elementSize;
+                offset += ElementSize.Value;
             }
 
             return buffer.ToArray();
 
         }
 
-        virtual protected void EncodeArray(Tag tag, T[] values, int elementSize)
+        virtual protected void EncodeArray(Tag tag, T[] values)
         {
+            if (ElementSize is null)
+            {
+                throw new ArgumentNullException($"{nameof(ElementSize)} cannot be null for array encoding");
+            }
+
             int offset = 0;
             foreach (var item in values)
             {
                 Encode(tag, offset, item);
-                offset += elementSize;
+                offset += ElementSize.Value;
             }
         }
 
         virtual public T Decode(Tag tag) => Decode(tag, 0);
         public abstract T Decode(Tag tag, int offset);
 
+
         virtual public void Encode(Tag tag, T value) => Encode(tag, 0, value);
         public abstract void Encode(Tag tag, int offset, T value);
 
+        virtual public void Encode(Tag tag, T[] value) => EncodeArray(tag, value);
+
+        T[] IMarshaller<T[]>.Decode(Tag tag) => DecodeArray(tag);
     }
 
 }
