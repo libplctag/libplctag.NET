affa542850abfb18e971b12769e563f80d610dfd
jkoplo <jkoplo@users.noreply.github.com>
2020-08-18 01:09:06 -0700

    Format indents

--- src/libplctag/DataTypes/IMarshaller.cs
+++ src/libplctag/DataTypes/IMarshaller.cs
@@ -10,29 +10,29 @@
         PlcType PlcType { get; set; }
 
 
-        /// <summary>
-        /// Provide an integer value for ElementSize if you
-        /// want to pass this into the tag constructor
-        /// </summary>
-        int? ElementSize { get; }
-        //PlcType PlcType { get; set; }
+    /// <summary>
+    /// Provide an integer value for ElementSize if you
+    /// want to pass this into the tag constructor
+    /// </summary>
+    int? ElementSize { get; }
+    //PlcType PlcType { get; set; }
 
-        ///// <summary>
-        ///// This will return the number of items defined in a tag 
-        ///// </summary>
-        int? GetArrayLength(Tag tag);
+    ///// <summary>
+    ///// This will return the number of items defined in a tag 
+    ///// </summary>
+    int? GetArrayLength(Tag tag);
 
-        /// <summary>
-        /// This is used to convert the number of array elements
-        /// into the raw element count, which is used by the library.
-        /// Most of the time, this will be the same value, but occasionally
-        /// it is not (e.g. BOOL arrays).
-        /// </summary>
-        int? SetArrayLength(int? elementCount);
+    /// <summary>
+    /// This is used to convert the number of array elements
+    /// into the raw element count, which is used by the library.
+    /// Most of the time, this will be the same value, but occasionally
+    /// it is not (e.g. BOOL arrays).
+    /// </summary>
+    int? SetArrayLength(int? elementCount);
 
-        T Decode(Tag tag);
-        //T DecodeOne(Tag tag, int offset, out int elementSize);
-        void Encode(Tag tag, T value);
-        //void EncodeOne(Tag tag, int offset, out int elementSize, T value);
-    }
+    T Decode(Tag tag);
+    //T DecodeOne(Tag tag, int offset, out int elementSize);
+    void Encode(Tag tag, T value);
+    //void EncodeOne(Tag tag, int offset, out int elementSize, T value);
+}
 }
