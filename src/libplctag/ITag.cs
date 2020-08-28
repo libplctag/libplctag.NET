c6573155e124235ce0642079cf45e3fe9f755464
jkoplo <jkoplo@users.noreply.github.com>
2020-08-27 22:20:55 -0700

    Generate the ITag interface

---
+++ src/libplctag/ITag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace libplctag
+{
+    public interface ITag : IDisposable
+    {
+        int[] ArrayDimensions { get; set; }
+        string Gateway { get; set; }
+        string Name { get; set; }
+        string Path { get; set; }
+        PlcType? PlcType { get; set; }
+        Protocol? Protocol { get; set; }
+        int? ReadCacheMillisecondDuration { get; set; }
+        TimeSpan Timeout { get; set; }
+        bool? UseConnectedMessaging { get; set; }
+
+        Status GetStatus();
+        void Initialize();
+        Task InitializeAsync(CancellationToken token = default);
+        void Read();
+        Task ReadAsync(CancellationToken token = default);
+        void Write();
+        Task WriteAsync(CancellationToken token = default);
+    }
+}
