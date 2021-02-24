4f83399ce0989fa7a7ffbc4c2334ea6d20475ebe
timyhac <timyhac@gmail.com>
2021-02-24 18:12:49 +1100

    Updated script to download the files as well as copy them

--- src/copyReleasesIntoNativeImport.ps1
+++ src/copyReleasesIntoNativeImport.ps1
@@ -1,13 +1,8 @@
 
 # This script is used to make it easier to copy native libraries into the libplctag.NativeImport project without error.
 
-
-# Step 1: Download the releases and put in a folder called "releases" next to this script. The folder structure should look like this:
-#   .\releases
-#   .\releases\libplctag_[version]_[os1]_[architecture1].zip
-#   .\releases\libplctag_[version]_[os2]_[architecture2].zip
-#   .\releases\libplctag_[version]_[os3]_[architecture3].zip
-#   ...
+# Step 1: Update the version variable with the version to download and copy into NativeImport
+$version = "2.3.4"
 
 # Step 2: Run the script
 
@@ -23,17 +18,15 @@
 
 
 
-
-
-$macos_x64 = @{     zip = "libplctag_*_macos_x64.zip";                  source = "libplctag_*_macos_x64\libplctag.dylib";                       destination = "osx_x64\libplctag.dylib" }
-$linux_x64 = @{     zip = "libplctag_*_ubuntu_x64.zip";                 source = "libplctag_*_ubuntu_x64\libplctag.so";                         destination = "linux_x64\libplctag.so" }
-$linux_x86 = @{     zip = "libplctag_*_ubuntu_x86.zip";                 source = "libplctag_*_ubuntu_x86\libplctag.so";                         destination = "linux_x86\libplctag.so" }
-$linux_ARM = @{     zip = "libplctag_*_linux_arm7l_EXPERIMENTAL.zip";   source = "libplctag_*_linux_arm7l_EXPERIMENTAL\libplctag.so";           destination = "linux_ARM\libplctag.so" }
-$linux_ARM64 = @{   zip = "libplctag_*_linux_aarch64_EXPERIMENTAL.zip"; source = "libplctag_*_linux_aarch64_EXPERIMENTAL\libplctag.so";         destination = "linux_ARM64\libplctag.so" }
-$windows_x64 = @{   zip = "libplctag_*_windows_x64.zip";                source = "libplctag_*_windows_x64\Release\plctag.dll";                  destination = "win_x64\plctag.dll" }
-$windows_x86 = @{   zip = "libplctag_*_windows_x86.zip";                source = "libplctag_*_windows_x86\Release\plctag.dll";                  destination = "win_x86\plctag.dll" }
-$windows_ARM = @{   zip = "libplctag_*_windows_Arm.zip";                source = "libplctag_*_windows_Arm\Release\plctag.dll";                  destination = "win_ARM\plctag.dll" }
-$windows_ARM64 = @{ zip = "libplctag_*_windows_Arm64_EXPERIMENTAL.zip"; source = "libplctag_*_windows_Arm64_EXPERIMENTAL\Release\plctag.dll";   destination = "win_ARM64\plctag.dll" }
+$macos_x64 = @{     zip = "libplctag_$($version)_macos_x64.zip";                  source = "libplctag_$($version)_macos_x64\libplctag.dylib";                       destination = "osx_x64\libplctag.dylib" }
+$linux_x64 = @{     zip = "libplctag_$($version)_ubuntu_x64.zip";                 source = "libplctag_$($version)_ubuntu_x64\libplctag.so";                         destination = "linux_x64\libplctag.so" }
+$linux_x86 = @{     zip = "libplctag_$($version)_ubuntu_x86.zip";                 source = "libplctag_$($version)_ubuntu_x86\libplctag.so";                         destination = "linux_x86\libplctag.so" }
+$linux_ARM = @{     zip = "libplctag_$($version)_linux_arm7l_EXPERIMENTAL.zip";   source = "libplctag_$($version)_linux_arm7l_EXPERIMENTAL\libplctag.so";           destination = "linux_ARM\libplctag.so" }
+$linux_ARM64 = @{   zip = "libplctag_$($version)_linux_aarch64_EXPERIMENTAL.zip"; source = "libplctag_$($version)_linux_aarch64_EXPERIMENTAL\libplctag.so";         destination = "linux_ARM64\libplctag.so" }
+$windows_x64 = @{   zip = "libplctag_$($version)_windows_x64.zip";                source = "libplctag_$($version)_windows_x64\Release\plctag.dll";                  destination = "win_x64\plctag.dll" }
+$windows_x86 = @{   zip = "libplctag_$($version)_windows_x86.zip";                source = "libplctag_$($version)_windows_x86\Release\plctag.dll";                  destination = "win_x86\plctag.dll" }
+$windows_ARM = @{   zip = "libplctag_$($version)_windows_Arm.zip";                source = "libplctag_$($version)_windows_Arm\Release\plctag.dll";                  destination = "win_ARM\plctag.dll" }
+$windows_ARM64 = @{ zip = "libplctag_$($version)_windows_Arm64_EXPERIMENTAL.zip"; source = "libplctag_$($version)_windows_Arm64_EXPERIMENTAL\Release\plctag.dll";   destination = "win_ARM64\plctag.dll" }
 
 
 
@@ -51,7 +44,7 @@ $builds = @(
 )
 
 
-
+$downloadUriRoot = "https://github.com/libplctag/libplctag/releases/download/v$($version)"
 $sourceFolder = ".\releases"
 $destinationFolder = ".\libplctag.NativeImport\runtime"
 
@@ -59,15 +52,33 @@ $destinationFolder = ".\libplctag.NativeImport\runtime"
 
 
 
-
-# Unzip the build files
+# Create a folder to put the builds and enter into it
+Remove-Item $sourceFolder -Recurse -Force
+New-Item -ItemType "directory" -Name $sourceFolder
 Push-Location
 Set-Location $sourceFolder
 
+
+
+
+# Download the builds
+foreach ($build in $builds) {
+	$uri = "$downloadUriRoot/$($build.zip)"
+    Invoke-WebRequest -Uri $uri -OutFile $build.zip
+}
+
+
+
+
+# Unzip the build files
 foreach ($build in $builds) {
 	Get-Item $build.zip      | Expand-Archive
 }
 
+
+
+
+# Exit out of the builds folder
 Pop-Location
 
 
