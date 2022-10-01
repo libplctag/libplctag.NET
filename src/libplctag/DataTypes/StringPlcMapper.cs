b9a8adc402d1347795f7efbbbf154df94fe38e4b
jfontsaballs <jfontsaballs@gmail.com>
2022-10-01 13:17:29 +0200

    Undo changes in StringPlcMapper.

--- src/libplctag/DataTypes/StringPlcMapper.cs
+++ src/libplctag/DataTypes/StringPlcMapper.cs
@@ -1,12 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace libplctag.DataTypes
 {
     public class StringPlcMapper : PlcMapperBase<string>
     {
+
         override public int? ElementSize
         {
             get
@@ -19,58 +19,14 @@ namespace libplctag.DataTypes
                     case PlcType.LogixPccc: return 84;
                     case PlcType.Micro800: return 256; //To be Confirmed
                     case PlcType.MicroLogix: return 84;
-                    case PlcType.Omron: return 256; //To be Confirmed
                     default: throw new NotImplementedException();
                 }
             }
         }
 
-        override public string Decode(Tag tag, int offset)
-        {
-            if (PlcType == PlcType.Omron)
-            {
-                var bytes = tag.GetBuffer();
-                if (bytes.Length <= 2)
-                    return "";
-                else
-                    return Encoding.ASCII.GetString(bytes, offset + 2, bytes.Length - 2 - offset);
-            }
-            else
-            {
-                return tag.GetString(offset);
-            }
-        }
 
-        override public void Encode(Tag tag, int offset, string value)
-        {
-            if (PlcType == PlcType.Omron)
-            {
-                if (value.Length == 0)
-                {
-                    tag.SetUInt16(0, 0);
-                }
-                else
-                {
-                    if (value.Length > 255)
-                        value = value.Remove(255);
-                    
-                    var bytes = new byte[] { Convert.ToByte(value.Length), 0 }
-                        .Concat(Encoding.ASCII.GetBytes(value))
-                        .ToList();
-                    
-                    if (bytes.Count % 2 == 1)
-                    {
-                        bytes.Add(0);
-                        bytes[0]++;
-                    }
+        override public string Decode(Tag tag, int offset) => tag.GetString(offset);
+        override public void Encode(Tag tag, int offset, string value) => tag.SetString(offset, value);
 
-                    tag.SetBuffer(bytes.ToArray());
-                }
-            }
-            else
-            {
-                tag.SetString(offset, value);
-            }
-        }
     }
 }
