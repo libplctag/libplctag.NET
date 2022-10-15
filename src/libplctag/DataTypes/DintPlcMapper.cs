6ed904a8d01ed7626ad03c6aca9eb3ae99c7a56e
timyhac <timyhac@gmail.com>
2022-10-15 15:53:54 +1100

    Added copyright notices to all source files

--- src/libplctag/DataTypes/DintPlcMapper.cs
+++ src/libplctag/DataTypes/DintPlcMapper.cs
@@ -1,4 +1,11 @@
-﻿namespace libplctag.DataTypes
+﻿// Copyright (c) libplctag.NET contributors
+// https://github.com/libplctag/libplctag.NET
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace libplctag.DataTypes
 {
     public class DintPlcMapper : PlcMapperBase<int>
     {
